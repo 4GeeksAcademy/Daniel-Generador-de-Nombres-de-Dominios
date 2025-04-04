9a0153c558e8f8c8b075b783eae02543f73f7b21
Daniel Henríquez <daniel.henriquez@outlook.com>
2025-04-04 19:32:04 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-import "bootstrap";
-import "./style.css";
+let array1 = ['the', 'our'];
+let array2 = ['great', 'big'];
+let array3 = ['jogger', 'racoon'];
 
+let todasLasCombinaciones = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+for (let elemento1 of array1) {
+    for (let elemento2 of array2) {
+        for (let elemento3 of array3) {
+            todasLasCombinaciones.push(elemento1 + elemento2 + elemento3 + ".com");
+        }
+    }   
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+console.log(todasLasCombinaciones);
